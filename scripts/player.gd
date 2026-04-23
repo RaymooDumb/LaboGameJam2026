594fcd54146dc15853d3dab2c7e711e984c59e67
raymoo <holacirno@gmail.com>
2026-04-23 01:55:10 -0600

    Escena de Prueba bien y Animaciones
    Animaciones y bg importados jeje

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,17 +1,30 @@
 extends CharacterBody2D
 var speed = 300
 
+var rightLook : bool = true
+
 func _physics_process(_delta):
 	var direction = Vector2.ZERO
-
+	
+	if velocity != Vector2(0,0):
+		$AnimatedSprite2D.play("walk")
+		if rightLook == true:
+			$AnimatedSprite2D.flip_h = false
+		else:
+			$AnimatedSprite2D.flip_h = true
+	else:
+		$AnimatedSprite2D.play("standing")
+	
 	if Input.is_action_pressed("Right"):
+		rightLook = true
 		direction.x += 1
 	elif Input.is_action_pressed("Left"):
+		rightLook = false
 		direction.x -= 1
-	elif Input.is_action_pressed("Down"):
-		direction.y += 1
-	elif Input.is_action_pressed("Up"):
-		direction.y -= 1
+	#elif Input.is_action_pressed("Down"):
+		#direction.y += 1
+	#elif Input.is_action_pressed("Up"):
+		#direction.y -= 1
 
-	velocity = direction.normalized() * speed
+	velocity = direction * speed
 	move_and_slide()
